f8d7f570571929654ed83a75082b045a55cf0bb3
Mukhammadkarim Tukhtaboyev <75162831+MrTukhtaboyev@users.noreply.github.com>
2022-04-13 09:36:21 +0500

    Basic Auth

--- EducationCenterUoW.Service/Helpers/HttpContextHelper.cs
+++ EducationCenterUoW.Service/Helpers/HttpContextHelper.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Text;
 
 namespace EducationCenterUoW.Service.Helpers
 {
@@ -8,5 +10,24 @@ namespace EducationCenterUoW.Service.Helpers
         public static HttpContext Context => Accessor?.HttpContext;
         public static IHeaderDictionary ResponseHeaders => Context?.Response?.Headers;
         public static string Language => Context?.Request?.Headers["Accept-Language"].ToString();
+
+        /// <summary>
+        /// Basic authentication
+        /// </summary>
+        private static string BasicAuth => Context?.Request?.Headers["Authorization"].ToString();
+        public static string BasicUsername => GetBasicCredentials().username;
+        public static string BasicPassword => GetBasicCredentials().password;
+
+        private static (string username, string password) GetBasicCredentials()
+        {
+            string[] basic = BasicAuth.Split(' ');
+            if(basic.Length != 2)
+               return (string.Empty, string.Empty);
+
+            byte[] data = Convert.FromBase64String(basic[1]);
+            string decodedString = Encoding.UTF8.GetString(data);
+
+            return (decodedString.Split(':')[0], decodedString.Split(':')[1]);
+        }
     }
 }
